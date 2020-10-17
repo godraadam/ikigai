cc35ff00a9627dbe929978a8df1b7f8059852399
Godra Adam <godra.adam@gmail.com>
2020-10-17 17:41:26 +0300

    class renames

--- ikigai-server/src/main/java/dev/godraadam/ikigai/server/ServerApplication.java
+++ ikigai-server/src/main/java/dev/godraadam/ikigai/server/ServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IkigaiApplication {
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IkigaiApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
