d4202f538efebdcd9e94aa535c66a8ff7031af7e
Godra Adam <godra.adam@gmail.com>
2020-10-17 12:11:26 +0300

    corrected package declaration

--- ikigai-server/src/main/java/dev/godraadam/ikigai/server/IkigaiApplication.java
+++ ikigai-server/src/main/java/dev/godraadam/ikigai/server/IkigaiApplication.java
@@ -1,4 +1,4 @@
-package dev.godraadam.ikigai;
+package dev.godraadam.ikigai.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
