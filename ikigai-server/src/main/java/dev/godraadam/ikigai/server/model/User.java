8f3f9070324a8755ca550c64acaf60e88d4342ed
Godra Adam <godra.adam@gmail.com>
2020-10-17 17:38:26 +0300

    added model layer to backend

---
+++ ikigai-server/src/main/java/dev/godraadam/ikigai/server/model/User.java
@@ -0,0 +1,83 @@
+package dev.godraadam.ikigai.server.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ikigai_user")
+public class User extends BaseModel {
+    
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String passWord;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+	private List<Visit> visits;
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the passWord
+     */
+    public String getPassWord() {
+        return passWord;
+    }
+
+    /**
+     * @param passWord the passWord to set
+     */
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the visits
+     */
+    public List<Visit> getVisits() {
+        return visits;
+    }
+
+    /**
+     * @param visits the visits to set
+     */
+    public void setVisits(List<Visit> visits) {
+        this.visits = visits;
+    }
+
+}
---
+++ ikigai-server/src/main/java/dev/godraadam/ikigai/server/model/Visit.java
@@ -0,0 +1,85 @@
+package dev.godraadam.ikigai.server.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity 
+@Table
+public class Visit extends BaseModel {
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USER_ID")
+    private User user;
+
+    @Column
+    private String country;
+
+    @Column
+    private String description;
+
+    @Column
+    private Date date;
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
