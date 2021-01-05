85c968a044e705c0d784e0e2a0a5d8e3fedb39cc
Godra Adam <godra.adam@gmail.com>
2021-01-05 08:35:11 +0200

    more sql imports

--- ikigai-server/src/main/resources/import.sql
+++ ikigai-server/src/main/resources/import.sql
@@ -1,1 +1,8 @@
 insert into ikigai_user (user_name, pass_word, email) values ("godraadam", "asdfgh", "g.a@dev.com");
+insert into ikigai_user (user_name, pass_word, email) values ("godraadam2", "asdfgh", "g.a2@dev.com");
+insert into ikigai_user (user_name, pass_word, email) values ("benedekbalazs", "asdfgh", "bb@dev.com");
+insert into ikigai_user (user_name, pass_word, email) values ("benedekbalazs2", "asdfgh", "bb2@dev.com");
+insert into ikigai_user (user_name, pass_word, email) values ("benbal", "asdfgh", "blasio@dev.com");
+insert into ikigai_user (user_name, pass_word, email) values ("godada", "asdfgh", "cobra@dev.com");
+insert into visit (country, description, date) values ("Romania", "My homeland <3", "01/10/1999");
+insert into ikigai_user_visits (user_id, visits_id) values (1, 1);
